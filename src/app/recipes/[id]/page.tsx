91ea8d62fd4df515f37999407e1be1d4520ee482
Jon Wyatt <jonnywyatt2@gmail.com>
2023-05-08 23:33:27 +0100

    Responsive layout for recipe page

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -2,7 +2,6 @@ import { getData } from '@/app/utils/getData';
 import styles from './recipe.module.scss';
 import grid from '@/app/styles/grid.module.css';
 import spacing from '../../styles/spacing.module.css';
-import colours from '../../styles/colours.module.css';
 import Image from 'next/image';
 import { Tags } from '@/components/Tags';
 import { Tab, TabList, TabPanel, Tabs } from '@/components/Tabs';
@@ -21,26 +20,26 @@ export default async function Recipe({ params }: PageProps) {
           <Tags tags={recipe.tags} />
         </div>
       </div>
-      <div className={grid.fullBleed}>
-        <div
-          className={`${spacing.paddingBottom4} ${colours.backgroundSecondary}`}
-        >
+      <div className={`${grid.fullBleed} ${styles.contentWrapper}`}>
+        <div className={styles.imageContainer}>
           <div className={styles.imageWrapper}>
             <img className={styles.image} src={recipe.src} alt={recipe.title} />
           </div>
         </div>
-        <Tabs defaultSelectedTab="tab1">
-          <TabList aria-label="jser tabs">
-            <Tab tab="tab1">Ingredients</Tab>
-            <Tab tab="tab2">Method</Tab>
-          </TabList>
-          <TabPanel tab="tab1">
-            <List list={recipe.ingredients.main} />
-          </TabPanel>
-          <TabPanel tab="tab2">
-            <List list={recipe.steps} />
-          </TabPanel>
-        </Tabs>
+        <div className={styles.content}>
+          <Tabs defaultSelectedTab="tab1">
+            <TabList aria-label="jser tabs">
+              <Tab tab="tab1">Ingredients</Tab>
+              <Tab tab="tab2">Method</Tab>
+            </TabList>
+            <TabPanel tab="tab1">
+              <List list={recipe.ingredients.main} />
+            </TabPanel>
+            <TabPanel tab="tab2">
+              <List list={recipe.steps} />
+            </TabPanel>
+          </Tabs>
+        </div>
       </div>
     </>
   );
