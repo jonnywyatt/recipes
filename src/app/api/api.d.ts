995c5a359a6545eea901871959ba523a2f7146b8
Jon Wyatt <jonnywyatt2@gmail.com>
2023-05-15 21:43:32 +0100

    TS fix

--- src/app/api/api.d.ts
+++ src/app/api/api.d.ts
@@ -10,10 +10,15 @@ export interface DecoratedTag extends Api.Tag {
   href: string;
 }
 
+interface Image {
+  fileName: string;
+  width: number;
+}
+
 export interface Recipe {
   id: string;
   title: string;
-  src: string;
+  images: Image[];
   tags: Tag[];
   ingredients: {
     main: Ingredient[];
