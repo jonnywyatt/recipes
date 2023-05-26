bfff3a4670e7ea471b1fbeb1d38dd56bb43364ed
Jon Wyatt <jonnywyatt2@gmail.com>
2023-05-26 23:42:56 +0100

    Loading

--- src/app/(recipes)/loading.tsx
+++ src/app/(recipes)/loading.tsx
@@ -1,5 +1,11 @@
 import grid from '@/app/styles/grid.module.css';
+import { inspect } from 'util';
+import styles from './page.module.scss';
 
 export default function Loading() {
-  return <div className={grid.gridColumnCenter}>Loading...</div>;
+  return (
+    <div className={grid.gridColumnCenter}>
+      <div className={styles.loading}>Loading...</div>
+    </div>
+  );
 }
