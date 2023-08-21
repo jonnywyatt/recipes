a9ce661ac58f263a0e47354a7bc54019909b2049
Jon Wyatt <jonnywyatt2@gmail.com>
2023-08-21 22:42:45 +0100

    Plants page copy / image

--- src/app/plants/page.tsx
+++ src/app/plants/page.tsx
@@ -6,19 +6,53 @@ export default async function PlantsInfo() {
   return (
     <main className={grid.gridColumnCenter}>
       <h1 className={styles.heading}>What counts as a plant ingredient?</h1>
-      <div className={styles.textBlock}>
-        <p>It’s not just fruits and veggies that count.</p>
-        <p>You can include grains, nuts, seeds, herbs, and spices, too</p>
-        <p>
-          The Zoe project website has more details on{' '}
-          <a
-            className={buttonsLinks.textLink}
-            href={'https://joinzoe.com/learn/30-plants-per-week#what-counts?'}
-          >
-            what counts as a plant
-          </a>
-          .
-        </p>
+      <div className={styles.content}>
+        <div className={styles.imageWrapper}>
+          <img
+            className={styles.image}
+            src={'/images/spices.jpg'}
+            alt={'Spices'}
+          />
+        </div>
+        <div className={styles.textBlock}>
+          <p>We all know that eating plants is good for us.</p>
+          <p>
+            However, it seems that eating a large <strong>variety</strong> of
+            plant foods is even better. Eating many different plant foods
+            improves our gut health by encouraging the growth of different
+            species of bacteria that live there, especially the healthy,
+            beneficial (good) bacteria.
+          </p>
+          <p>
+            The{' '}
+            <a
+              className={buttonsLinks.textLink}
+              href={'https://joinzoe.com/learn/30-plants-per-week#what-counts?'}
+            >
+              recommendation by the Zoe project
+            </a>{' '}
+            is to eat at least 30 different plant-based foods per week.
+          </p>
+          <p>
+            But it's not as daunting as it sounds - a wide range of foods count
+            as plant-based including grains, nuts, seeds, herbs, and spices.
+          </p>
+          <p>
+            Portion size doesn't really matter; it's the variety of different
+            plant-based foods in your diet, that matters. A pinch of dried herbs
+            counts the same as plateful of salad.
+          </p>
+          <p>
+            The Zoe project website has more details on{' '}
+            <a
+              className={buttonsLinks.textLink}
+              href={'https://joinzoe.com/learn/30-plants-per-week#what-counts?'}
+            >
+              the foods that count towards your 30 plants per week target
+            </a>
+            .
+          </p>
+        </div>
       </div>
     </main>
   );
